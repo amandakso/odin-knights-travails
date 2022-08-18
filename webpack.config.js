a62ba87311c4c9837be488db844d2fdd347366a7
Amanda So <soamanda95@gmail.com>
2022-08-17 22:27:31 -0700

    update devtools

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
 
     entry: './src/index.js',
     mode: 'development',
+    devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
